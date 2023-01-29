9b39cdd30f44f4db0b5b98bfa4da88b4730ecc0d
SHRIRAM <nshriram1326@gmail.com>
2023-01-29 12:08:33 +0530

    main.py added not working properly

---
+++ main.py
@@ -0,0 +1,16 @@
+from flask import Flask, render_template, request
+app = Flask(__name__)
+
+@app.route("/", methods=["GET", "POST"])
+def calculator():
+    if request.method == "POST":
+        expression = request.form.get("expression")
+        try:
+            result = eval(expression)
+        except:
+            result = "Invalid expression"
+        return render_template("calci.html", expression=expression, result=result)
+    return render_template("calci.html")
+
+if __name__ == "__main__":
+    app.run(port=8080)
